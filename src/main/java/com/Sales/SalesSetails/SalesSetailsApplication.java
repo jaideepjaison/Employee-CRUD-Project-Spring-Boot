b20ffb800b53bd657c0b0b8a6dee8928696972d1
jaideepjaison <jaison3666@gmail.com>
2024-01-10 12:16:21 +0530

    application.properties updated

--- src/main/java/com/Sales/SalesSetails/SalesSetailsApplication.java
+++ src/main/java/com/Sales/SalesSetails/SalesSetailsApplication.java
@@ -3,9 +3,11 @@ package com.Sales.SalesSetails;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.ComponentScan;
+import org.springframework.web.bind.annotation.CrossOrigin;
 
 
 @SpringBootApplication
+@CrossOrigin(origins = "https://659c331662d228008e1954e2--statuesque-crisp-a2214d.netlify.app/")
 
 public class SalesSetailsApplication {
 
